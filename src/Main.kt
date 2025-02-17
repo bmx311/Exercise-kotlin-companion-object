f00e59adb9a5a2b64ba8ea6fca17c2426e929962
abdulwahab alsenan <ak-alsenan@hotmail.com>
2025-02-17 19:02:09 +0300

    task complete

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,19 @@
+class TemperatureConverter{
+    companion object {
+        fun toCelsius(fahrenheit: Double): Double {
+            return (fahrenheit - 32) * 5/9
+        }
+
+        fun toFahrenheit(celsius: Double): Double {
+            return celsius * 9/5 + 32
+        }
+    }
+}
+
 fun main() {
-    println("Hello World!")
+    val tempF = 789.242
+    val tempC = 20.789
+
+    println("$tempF°F = ${TemperatureConverter.toCelsius(tempF)}°C")
+    println("$tempC°C = ${TemperatureConverter.toFahrenheit(tempC)}°F")
 }
